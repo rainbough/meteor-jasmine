b92bb17275e6817cd8f72dbe683028461f12f079
rainbough <rainbough@gmail.com>
2014-05-01 10:24:48 -0500

    initial commit

---
+++ packages/jasmine/package.js
@@ -0,0 +1,9 @@
+Package.describe({
+	summary: "Jasmine testing packaged for meteor"
+});
+
+Package.on_use(function(api) {
+	api.use(["underscore", "templating"], "client");
+	api.add_files(["jasmine.js", "jasmine-html.js", "boot.js", "console.js", "templates.html", "jasmine.css", "jasmine_favicon.png", "SpecRunner.html"], "client");
+});
+
---
+++ packages/jasmine/specRunner.js
@@ -0,0 +1,3 @@
+// Template.specRunner.rendered = function(){
+	
+// }
